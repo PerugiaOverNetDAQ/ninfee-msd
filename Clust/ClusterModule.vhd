71e1048894699f40edd8e8822efd6e412b203b34
Luca Russo <liuk.qwerty@gmail.com>
2026-08-06 15:55:26 +0200

    Fix to ClusterModule BUG

--- Clust/ClusterModule.vhd
+++ Clust/ClusterModule.vhd
@@ -374,11 +374,15 @@ begin
               -- sicurezza per non interrompere un cluster con una strip morta.
               if signed(iRD_DATA) > signed(iHT) then
                 vNewP0(cPIPE_HT_BIT) := '1';
-              elsif iFLG /= "0000" then
-                vNewP0(cPIPE_HT_BIT) := '1';
               end if;
 
-              if signed(iRD_DATA) > signed(iLT) and iFLG = "0000" then
+              if signed(iRD_DATA) > signed(iLT) then
+                vNewP0(cPIPE_LT_BIT) := '1';
+              end if;
+
+              -- Se i flag non sono tutti a zero, alzo LT e abbasso HT
+              if iFLG /= "0000" then
+                vNewP0(cPIPE_HT_BIT) := '0';
                 vNewP0(cPIPE_LT_BIT) := '1';
               end if;
 
